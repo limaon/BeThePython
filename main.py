50a7f0665f3559549cb3e6f19c3d930db7128fe0
Alvaro Lima <alvarolima1103@outlook.com>
2020-05-31 19:18:35 -0300

    Estudando Strings

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello World")
+
+#print("Hello World")
+
+
+#Strings
+
+nome_do_carro = "celta"
+nome_da_placa = "Placa de parar"
+print(nome_da_placa)
+
+#Caracters de Escape
+resenha_do_fime = "O filme foi \"demais\""
+print(resenha_do_fime)
+
+#Formatando Strings
+aluno = 'junior'
+curso = 'Fotografia'
+men = 'Nome: ' + aluno + 'Curso de ' + curso
+print(men)
+
+
+animal = 'gato'
+print("Esse animail e um %s e ele anda" % animal)
+
+print("-----------")
+print("-----------")
+print("-----------")
+
+txt = "Para somente {price:.2f} reais"
+print(txt.format(price = 3))
