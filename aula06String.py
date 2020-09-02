e4fbc900132816d413a7609a5c06de63f50b2e48
Alvaro Lima <alvarolima1103@hotmail.com>
2020-09-01 20:39:01 -0400

    Pratica diaria

---
+++ aula06String.py
@@ -0,0 +1,15 @@
+curso = "Curso de Python"
+
+curso.strip()
+
+#print(curso[0:6])
+#print(curso.strip())
+#print(curso.lower().strip())
+#print(curso.upper())
+#print(curso.replace("Python", "C#"))
+
+a = curso.split(" ")
+print(a[2])
+
+print("Tamanho: " + str(len(curso)))
+
