49bf117a9e11baeba1bbba3e5cdc8d8af8ab55da
Alvaro Lima <alvarolima1103@outlook.com>
2021-06-04 23:05:07 -0400

    Day 5 Finished

---
+++ PyLoops/Adding_Even.py
@@ -0,0 +1,14 @@
+# Write your code below row
+
+total = 0
+for number in range(2, 101, 2):
+    total += number
+print(total)
+
+# Other solution
+total2 = 0
+for number in range(1, 101):
+    if number % 2 == 0:
+        total2 += number
+
+print(total2)
