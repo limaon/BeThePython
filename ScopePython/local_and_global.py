41b12d3d88254a1c0938178091c3fd95b27614db
Alvaro Oliveira <alvarolima1103@outlook.com>
2021-06-10 20:54:51 -0400

    Day 11 Finished

---
+++ ScopePython/local_and_global.py
@@ -0,0 +1,36 @@
+##### Scope 
+
+#  enemies = 1 
+#  
+#  def increasse_enemies():
+    #  enemies = 2
+    #  print(f"enemies inside funtion: {enemies}")
+#  
+#  increasse_enemies()
+#  print(f"enemies outside function {enemies}")
+
+# Local Scope
+#  def drink_potion():
+    #  potion_strengh = 2
+    #  print(potion_strengh)
+#  
+#  drink_potion()
+#  print(potion_strengh)
+
+# Global Scope
+#  player_helth = 10
+#  
+#  def game():
+    #  def drink_potion():
+        #  potion_strengh = 2
+        #  print(potion_strengh)
+    #  drink_potion()
+#  
+#  print(player_helth)
+
+#  game_level = 3
+#  if game_level < 5:
+    #  enemies = ["Skeleton", "Zombie", "Alies"]
+    #  if game_level < 5:
+        #  new_enemy = enemies[0]
+    #  print(new_enemy)
